b9944cce6e19bca5f2a6b0a0e6c55269f45d98c6
AriaArcadia <107249147+AriaArcadia@users.noreply.github.com>
2025-05-22 11:54:47 +0800

    取消渐变效果

--- lib/screens/components/fade_image_widget.dart
+++ lib/screens/components/fade_image_widget.dart
@@ -8,21 +8,7 @@ class FadeImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ShaderMask(
-      shaderCallback: (Rect bounds) {
-        return const RadialGradient(
-          center: Alignment.center,
-          radius: 0.8,
-          colors: [
-            Colors.black,
-            Colors.black,
-            Colors.transparent,
-          ],
-          stops: [0.7, 0.95, 1.0], // 渐变范围
-        ).createShader(bounds);
-      },
-      blendMode: BlendMode.dstIn, // 混合模式，保留透明部分
-      child: child,
-    );
+    // 直接返回 child，不再应用 ShaderMask 和 RadialGradient
+    return child;
   }
 }
